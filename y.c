4bbce93455a9614b1118088e469d5023aa265e2a
obrovec913 <boroda9133@yandex.by>
2024-01-08 16:35:42 +0300

    yukj

--- y.c
+++ y.c
@@ -16,7 +16,7 @@ void encrypt_belt_cbc(const unsigned char *plaintext, size_t plaintext_len,
                       const unsigned char *key, const unsigned char *iv,
                       unsigned char **ciphertext, size_t *ciphertext_len)
 {
-     // Инициализация OpenSSL
+    // Инициализация OpenSSL
     OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_CIPHERS | OPENSSL_INIT_ADD_ALL_DIGESTS, NULL);
 
     // Загрузка встроенных движков
@@ -27,14 +27,14 @@ void encrypt_belt_cbc(const unsigned char *plaintext, size_t plaintext_len,
     OpenSSL_add_all_algorithms();
 
     // Загрузка вашей библиотеки (замените "/путь/к/вашей/библиотеке" на реальный путь)
-    if (!ENGINE_ctrl_cmd_string(NULL, "DIR_LOAD", "/home/on/bee2evp/build/local/lib", 0))
-    {
-        fprintf(stderr, "Failed to load the library: %s\n", ERR_error_string(ERR_get_error(), NULL));
-        handleErrors();
-    }
     
+
     ENGINE *engine = ENGINE_by_id("bee2evp");
-    if (!engine)
+    if (engine)
+    {
+        ENGINE_ctrl_cmd_string(engine, "DIR_LOAD", "/home/on/bee2evp/build/local/lib", 0);
+    }
+    else
     {
         fprintf(stderr, "Failed to load bee2evp engine: %s\n", ERR_error_string(ERR_get_error(), NULL));
         handleErrors();
