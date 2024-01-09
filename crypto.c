59f9f0a01b43acca9c9342c46ed66d6c2dbc9187
obrovec913 <boroda9133@yandex.by>
2024-01-09 17:32:20 +0300

    yukj

--- crypto.c
+++ crypto.c
@@ -62,15 +62,73 @@ int encryptWithOpenSSL(const char *input, const char *outputFile, const char *ke
     return EXIT_SUCCESS;
 }
 
+int decryptWithOpenSSL(const char *inputFile, const char *output, const char *key) {
+    char buffer[BUFFER_SIZE];
+    char keytu[BUFFER_SIZE];
+
+    // Открываем файл с ключом
+    FILE *keyFilePtr = fopen(key, "r");
+    if (keyFilePtr == NULL) {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+
+    // Считываем ключ из файла
+    if (fscanf(keyFilePtr, "%s", keytu) != 1) {
+        fprintf(stderr, "Error reading key from file.\n");
+        fclose(keyFilePtr);
+        return EXIT_FAILURE;
+    }
+
+    fclose(keyFilePtr);
+
+    // Формируем команду с использованием аргументов функции
+    char command[256];
+    snprintf(command, sizeof(command), "openssl enc -d -engine bee2evp -belt-cbc128 -in %s -out %s -k %s",
+             inputFile, output, keytu);
+
+    // Выполняем команду
+    if (system(command) == -1) {
+        perror("system");
+        return EXIT_FAILURE;
+    }
+
+    // Открываем дешифрованный файл и выводим его содержимое
+    FILE *decryptedFile = fopen(output, "r");
+    if (decryptedFile == NULL) {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+
+    printf("Decrypted contents:\n");
+
+    while (fgets(buffer, BUFFER_SIZE, decryptedFile) != NULL) {
+        printf("%s", buffer);
+    }
+
+    fclose(decryptedFile);
+
+    return EXIT_SUCCESS;
+}
+
 int main() {
     const char *inputFile = "мама мыла сашу";
-    const char *outputFile = "encrypted.txt";
+    const char *encryptedFile = "encrypted.txt";
+    const char *decryptedFile = "decrypted.txt";
     const char *key = "symmetric_key.txt";
 
-    if (encryptWithOpenSSL(inputFile, outputFile, key) == EXIT_SUCCESS) {
+    if (encryptWithOpenSSL(inputFile, encryptedFile, key) == EXIT_SUCCESS) {
         printf("Encryption successful.\n");
     } else {
         fprintf(stderr, "Encryption failed.\n");
+        return EXIT_FAILURE;
+    }
+
+    if (decryptWithOpenSSL(encryptedFile, decryptedFile, key) == EXIT_SUCCESS) {
+        printf("Decryption successful.\n");
+    } else {
+        fprintf(stderr, "Decryption failed.\n");
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
