b0a620ff30eaddaf66cf09ff73bb9bdbfc1e66e0
obrovec913 <boroda9133@yandex.by>
2024-01-09 14:40:24 +0300

    yukj

---
+++ crypto.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
+int encryptWithOpenSSL(const char *inputFile, const char *outputFile, const char *key) {
+    FILE *fp;
+    char buffer[BUFFER_SIZE];
+
+    // Формируем команду с использованием аргументов функции
+    char command[256];
+    snprintf(command, sizeof(command), "openssl  enc -engine bee2evp -belt-cbc128 -in %s -out %s -k cat(%s)", inputFile, outputFile, key);
+
+    // Открываем канал для выполнения команды и чтения её вывода
+    fp = popen(command, "r");
+    if (fp == NULL) {
+        perror("popen");
+        return EXIT_FAILURE;
+    }
+
+    // Читаем вывод команды из канала
+    while (fgets(buffer, BUFFER_SIZE, fp) != NULL) {
+        printf("%s", buffer);  // или сохраните в вашу переменную
+    }
+
+    // Закрываем канал
+    if (pclose(fp) == -1) {
+        perror("pclose");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int main() {
+    const char *inputFile = "README.md";
+    const char *outputFile = "encrypted.txt";
+    const char *key = "symmetric_key.txt";
+
+    if (encryptWithOpenSSL(inputFile, outputFile, key) == EXIT_SUCCESS) {
+        printf("Encryption successful.\n");
+    } else {
+        fprintf(stderr, "Encryption failed.\n");
+    }
+
+    return EXIT_SUCCESS;
+}
