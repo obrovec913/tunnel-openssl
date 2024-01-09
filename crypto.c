18464dcfff66f9e674a3a0a3c29fa92d20177b5a
obrovec913 <boroda9133@yandex.by>
2024-01-09 17:00:13 +0300

    yukj

--- crypto.c
+++ crypto.c
@@ -35,7 +35,7 @@ int encryptWithOpenSSL(const char *input, const char *outputFile, const char *ke
     FILE *tempFile = fdopen(tempFileDescriptor, "w");
     if (tempFile == NULL) {
         perror("fdopen");
-        close(tempFileDescriptor);
+        fclose(tempFileDescriptor);
         return EXIT_FAILURE;
     }
 
