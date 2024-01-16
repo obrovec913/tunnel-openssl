00e1cb1e0937e87d398d3f6464722abd3db926a5
obrovec913 <boroda9133@yandex.by>
2024-01-16 15:31:53 +0300

    yukj

--- alg.c
+++ alg.c
@@ -3,16 +3,20 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
+int print_cipher_names(const OBJ_NAME *obj_name) {
+    printf("%s\n", obj_name->name);
+    return 1;
+}
+
 int main() {
     SSL_library_init();
     OpenSSL_add_all_algorithms();
 
     printf("Available ciphers:\n");
 
-    OBJ_NAME_do_all(OBJ_NAME_TYPE_CIPHER_METH, NULL, [](const OBJ_NAME *obj_name) -> int {
-        printf("%s\n", obj_name->name);
-        return 1;
-    });
+    OBJ_NAME_do_all(OBJ_NAME_TYPE_CIPHER_METH, NULL, print_cipher_names);
+
+    ERR_print_errors_fp(stderr);  // Вывести ошибки OpenSSL
 
     return 0;
 }
