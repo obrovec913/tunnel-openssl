aadadaa92fcb04af87b4d158c64aff065549ea9e
obrovec913 <boroda9133@yandex.by>
2024-01-17 17:44:01 +0300

    yukj

--- uuu.c
+++ uuu.c
@@ -1,16 +1,21 @@
 #include <openssl/engine.h>
 #include <openssl/evp.h>
-//#include <openssl/bee2evp.h>
-int main() {
+// #include <openssl/bee2evp.h>
+int main()
+{
     // Инициализация OpenSSL и загрузка движка Bee2evp
     ENGINE_load_openssl();
- //   SSL_library_init();
+    //   SSL_library_init();
     ENGINE_load_builtin_engines();
     ENGINE_register_all_complete();
 
     // Получение указателя на движок Bee2evp
     ENGINE *engine = ENGINE_by_id("bee2evp");
-    if (!engine) {
+    if (!engine)
+    {
+        ERR_load_BEE2EVP_strings();
+        ERR_print_errors_fp(stderr);
+
         fprintf(stderr, "Ошибка: не удалось загрузить движок Bee2evp\n");
         return 1;
     }
@@ -21,6 +26,6 @@ int main() {
     // Освобождение ресурсов
     ENGINE_free(engine);
     EVP_cleanup();
-    
+
     return 0;
 }
