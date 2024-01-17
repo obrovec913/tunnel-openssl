c3db1d21f85c0773ab84aecf8aaac35fc0ca32bb
obrovec913 <boroda9133@yandex.by>
2024-01-17 16:11:21 +0300

    yukj

---
+++ uuu.c
@@ -0,0 +1,24 @@
+#include <openssl/engine.h>
+#include <openssl/evp.h>
+
+int main() {
+    // Инициализация OpenSSL и загрузка движка Bee2evp
+    ENGINE_load_builtin_engines();
+    ENGINE_register_all_complete();
+
+    // Получение указателя на движок Bee2evp
+    ENGINE *engine = ENGINE_by_id("bee2evp");
+    if (!engine) {
+        fprintf(stderr, "Ошибка: не удалось загрузить движок Bee2evp\n");
+        return 1;
+    }
+
+    // Использование движка Bee2evp для шифрования данных
+    // Ваш код для шифрования данных здесь
+
+    // Освобождение ресурсов
+    ENGINE_free(engine);
+    EVP_cleanup();
+    
+    return 0;
+}
