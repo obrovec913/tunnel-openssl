f2a53fc030bc66431802298e7037319905ab4091
obrovec913 <boroda9133@yandex.by>
2024-01-19 22:16:24 +0300

    yukj

--- uuu.c
+++ uuu.c
@@ -1,32 +1,32 @@
+#include <stdio.h>
 #include <openssl/engine.h>
-#include <openssl/evp.h>
-// #include <openssl/bee2evp.h>
-int main()
-{
-    // Инициализация OpenSSL и загрузка движка Bee2evp
-    ENGINE_load_openssl();
-    OpenSSL_add_all_algorithms();
-    //   SSL_library_init();
+
+int main() {
     ENGINE_load_builtin_engines();
     ENGINE_register_all_complete();
 
-    // Получение указателя на движок Bee2evp
-    ENGINE *engine = ENGINE_by_id("bee2evp");
-    if (!engine)
-    {
-        ERR_load_EVP_strings();
-        ERR_print_errors_fp(stderr);
-
-        fprintf(stderr, "Ошибка: не удалось загрузить движок Bee2evp\n");
-        return 1;
+    // Получаем список всех доступных движков
+    ENGINE *engine_list = ENGINE_get_first();
+    while (engine_list != NULL) {
+        printf("Доступный движок: %s\n", ENGINE_get_id(engine_list));
+        engine_list = ENGINE_get_next(engine_list);
     }
 
-    // Использование движка Bee2evp для шифрования данных
-    // Ваш код для шифрования данных здесь
+    // Теперь попробуем получить движок "bee2evp"
+    ENGINE *engine = ENGINE_by_id("bee2evp");
+    if (engine == NULL) {
+        fprintf(stderr, "Ошибка: не удалось загрузить движок Bee2evp\n");
+        ERR_print_errors_fp(stderr);
 
-    // Освобождение ресурсов
+        // Выведем список ошибок, чтобы увидеть, что происходит
+        ERR_print_errors_fp(stderr);
+        return -1;
+    }
+
+    printf("Движок успешно загружен: %s\n", ENGINE_get_id(engine));
+
+    // Тут можно использовать загруженный движок для выполнения необходимых задач
+
     ENGINE_free(engine);
-    EVP_cleanup();
-
     return 0;
 }
