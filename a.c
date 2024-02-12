6c167d5a32c720b2a665d7363607e1aa4c10d31a
obrovec913 <boroda9133@yandex.by>
2024-02-12 17:51:33 +0300

    yukj

--- a.c
+++ a.c
@@ -51,13 +51,16 @@ int main()
         handleErrors("Failed to create SSL context");
     }
     // Установка списка шифров
-    const char *ciphers = "belt-ecb128:belt-ecb192:belt-ecb256:belt-cbc128:belt-cbc192:belt-cbc256:belt-cfb128:belt-cfb192:belt-cfb256:belt-ctr128:belt-ctr192:belt-ctr256:belt-dwp128:belt-dwp192:belt-dwp256:belt-kwp128:belt-kwp192:belt-kwp256:bash256:bash384:bash512:belt-hash:bign-with-hspec:bign-with-hbelt:bign-with-bash256:bign-with-bash384:bign-with-bash512:bign-keytransport:bign-curve256v1:bign-curve384v1:bign-curve512v1:bign-primefield:bign-pubkey:belt-mac128:belt-mac192:belt-mac256:belt-hmac";
+    //const char *ciphers = "belt-ecb128:belt-ecb192:belt-ecb256:belt-cbc128:belt-cbc192:belt-cbc256:belt-cfb128:belt-cfb192:belt-cfb256:belt-ctr128:belt-ctr192:belt-ctr256:belt-dwp128:belt-dwp192:belt-dwp256:belt-kwp128:belt-kwp192:belt-kwp256:bash256:bash384:bash512:belt-hash:bign-with-hspec:bign-with-hbelt:bign-with-bash256:bign-with-bash384:bign-with-bash512:bign-keytransport:bign-curve256v1:bign-curve384v1:bign-curve512v1:bign-primefield:bign-pubkey:belt-mac128:belt-mac192:belt-mac256:belt-hmac";
+    const EVP_CIPHER *cipher = EVP_get_cipherbyname("belt-cbc128");
+    if (!cipher)
+        handleErrors();
 
-    if (!SSL_CTX_set_cipher_list(ssl_ctx, ciphers))
+    if (!SSL_CTX_set_cipher_list(ssl_ctx, cipher))
     {
         handleErrors("Failed to set cipher list");
     }
     return 0;
-    
+
 
 }
