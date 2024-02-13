64eb9f0e69cab2cefe2d625b5abcf7194b765869
obrovec913 <boroda9133@yandex.by>
2024-02-13 17:53:20 +0300

    yukj

--- a.c
+++ a.c
@@ -9,7 +9,19 @@ static void print_cipher(const char *name, void *arg) {
 int main() {
     OPENSSL_init_crypto(OPENSSL_INIT_ENGINE_ALL_BUILTIN | OPENSSL_INIT_LOAD_CONFIG, NULL);
     // Вывод всех доступных алгоритмов шифрования
-    EVP_CIPHER_names_do_all(NULL, print_cipher, NULL);
+    //EVP_CIPHER_names_do_all(NULL, print_cipher, NULL);
+     OpenSSL_add_all_algorithms();
 
+    // Имя алгоритма, который вы хотите инициализировать
+    const char *algorithm_name = "belt-cbc128"; // Например, алгоритм шифрования BEE2EVP
+
+    // Получение алгоритма из интерфейса EVP
+    const EVP_CIPHER *cipher = EVP_CIPHER_fetch(NULL, algorithm_name, NULL);
+
+    if (cipher == NULL) {
+        // Обработка ошибки
+        fprintf(stderr, "Failed to fetch cipher %s\n", algorithm_name);
+        return 1;
+    }
     return 0;
 }
