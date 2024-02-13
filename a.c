02c623262fe4b02dccc7845001e503732d2fc8cb
obrovec913 <boroda9133@yandex.by>
2024-02-13 16:11:09 +0300

    yukj

--- a.c
+++ a.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 // Функция обратного вызова, которая будет вызвана для каждого алгоритма
-static void print_cipher(const OBJ_NAME *obj, void *arg) {
+static void print_cipher(const OBJ_NAME *obj, void *arg)
+{
     printf("%s\n", obj->name);
 }
 
-int main() {
+int main()
+{
     // Вывод всех доступных алгоритмов шифрования
-    EVP_CIPHER_names_do_all(EVP_CIPHER_ALL, print_cipher, NULL);
+    EVP_CIPHER_names_do_all(NULL, print_cipher, NULL);
 
     return 0;
 }
