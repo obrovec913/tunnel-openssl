608fe69575dd4ccec7cc9a4b3a1a9d46e60115ed
obrovec913 <boroda9133@yandex.by>
2024-02-15 16:14:34 +0300

    yukj

--- n.c
+++ n.c
@@ -39,10 +39,35 @@ int main()
     {
         printf("Algorithm not found: %s\n", algorithm_name);
     }
-     printf("Algorithm name: %s\n", EVP_CIPHER_get0_name(cipher));
+    printf("Algorithm name: %s\n", EVP_CIPHER_get0_name(cipher));
     printf("Algorithm description: %s\n", EVP_CIPHER_get0_description(cipher));
     printf("Algorithm type: %d\n", EVP_CIPHER_get_type(cipher));
+    if (cipher != NULL)
+    {
+        int mode = EVP_CIPHER_mode(cipher);
 
+        const char *mode_str;
+        switch (mode)
+        {
+        case EVP_CIPH_CBC_MODE:
+            mode_str = "CBC";
+            break;
+        case EVP_CIPH_ECB_MODE:
+            mode_str = "ECB";
+            break;
+        case EVP_CIPH_CFB_MODE:
+            mode_str = "CFB";
+            break;
+        default:
+            mode_str = "Unknown";
+        }
+
+        printf("Algorithm: %s\nMode: %s\n", algorithm_name, mode_str);
+    }
+    else
+    {
+        printf("Algorithm not found: %s\n", algorithm_name);
+    }
 
     return 0;
 }
