ffe211c055975f0c9726a23dd640183df6fc2281
obrovec913 <boroda9133@yandex.by>
2024-02-15 14:49:55 +0300

    yukj

--- a.c
+++ a.c
@@ -20,9 +20,10 @@ int main()
    const EVP_CIPHER *cipheraa = EVP_get_cipherbyname(ciphersuites); 
     EVP_add_cipher(cipheraa);
 
+    const EVP_CIPHER *cipher
 
 
-   /* printf("Available ciphers:\n");
+    printf("Available ciphers:\n");
     for (int nid = 1; nid < 3000; nid++)
     {
         cipher = EVP_get_cipherbynid(nid);
@@ -33,15 +34,15 @@ int main()
         }
     }
 
-    */
+    
     //OpenSSL_add_all_algorithms();
 
     // Создание контекста SSL/TLS
     SSL_CTX *ssl_ctx = SSL_CTX_new(TLS_method());
 
     // Установка списка алгоритмов шифрования
-    const char *cipher = "belt-dwp-tls";
-    if (!SSL_CTX_set_cipher_list(ssl_ctx, cipher)) {
+    const char *cipherd = "belt-dwp-tls";
+    if (!SSL_CTX_set_cipher_list(ssl_ctx, cipherd)) {
         fprintf(stderr, "Failed to set cipher list\n");
         ERR_print_errors_fp(stderr);
         return 1;
