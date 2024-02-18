757ac7c3006de338283a8357de2bdfc7641057c3
obrovec913 <boroda9133@yandex.by>
2024-02-18 17:06:25 +0300

    yukj

--- aa.c
+++ aa.c
@@ -38,7 +38,7 @@ int init_my_engine()
 int main()
 {
     // Инициализировать OpenSSL
-    OPENSSL_init_crypto(OPENSSL_INIT_ENGINE_ALL_BUILTIN | OPENSSL_INIT_LOAD_CONFIG, NULL);
+    OPENSSL_init_crypto(OPENSSL_INIT_ENGINE_ALL_BUILTIN | OPENSSL_INIT_LOAD_CONFIG, OPENSSL_INIT_ADD_ALL_CIPHERS | OPENSSL_INIT_ADD_ALL_DIGESTS, NULL);
     SSL_library_init();
     SSL_load_error_strings();
     ENGINE_load_builtin_engines();
