becd02efd9567c4f4bc7786a7f041f29896e4289
obrovec913 <boroda9133@yandex.by>
2024-02-25 18:23:46 +0300

    first commit

--- aa.c
+++ aa.c
@@ -76,7 +76,7 @@ int main()
         printf("Algorithm belt-dwp256 is not available.\n");
     }
     // Создать SSL_CTX с вашим собственным движком
-    SSL_CTX *ctx = SSL_CTX_new(TTLS_method());
+    SSL_CTX *ctx = SSL_CTX_new(TLS_method());
     if (!ctx)
     {
         fprintf(stderr, "Failed to create SSL_CTX.\n");
