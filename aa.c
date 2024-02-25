370fe37ab974b9dc82283e97c523dafc278bc397
obrovec913 <boroda9133@yandex.by>
2024-02-25 19:17:18 +0300

    first commit

--- aa.c
+++ aa.c
@@ -1,111 +1,48 @@
-#include <openssl/evp.h>
-#include <stdio.h>
-#include <openssl/engine.h>
 #include <openssl/ssl.h>
 
-// Функция инициализации вашего собственного движка
-int init_my_engine()
-{
-    // Загрузка собственного движка
-    ENGINE *my_engine = ENGINE_by_id("bee2evp");
-    if (!my_engine)
-    {
-        fprintf(stderr, "Failed to load your engine.\n");
-        return 0;
-    }
+SSL_CTX *create_ssl_context() {
+    SSL_CTX *ctx;
+    
+    // Инициализация OpenSSL
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
 
-    // Инициализация собственного движка
-    if (!ENGINE_init(my_engine))
-    {
-        fprintf(stderr, "Failed to initialize your engine.\n");
-        ENGINE_free(my_engine);
-        return 0;
-    }
+    // Создание контекста SSL
+    ctx = SSL_CTX_new(TLSv1_2_method());
 
-    // Зарегистрировать собственный движок
-    if (!ENGINE_set_default(my_engine, ENGINE_METHOD_ALL))
-    {
-        fprintf(stderr, "Failed to register your engine.\n");
-        ENGINE_free(my_engine);
-        return 0;
-    }
-    // Зарегистрировать алгоритмы вашего движка
-    ENGINE_register_ciphers(my_engine);
+    // Загрузка сертификатов, ключей, параметров и т.д. если требуется
 
-    return 1;
+    // Установка параметров алгоритмов шифрования
+    SSL_CTX_set_cipher_list(ctx, "DHT-PSK-BIGN-WITH-BELT-CTR-MAC-HBELT:\
+        DHE-PSK-BIGN-WITH-BELT-CTR-MAC-HBELT:\
+        DHT-BIGN-WITH-BELT-CTR-MAC-HBELT");
+
+    // Установка параметров для ключа
+    SSL_CTX_set_psk_client_callback(ctx, psk_client_cb);
+    // Здесь psk_client_cb - функция, которая возвращает предварительно распределенный ключ (PSK)
+
+    return ctx;
 }
 
-int main()
-{
-    // Инициализировать OpenSSL
+int main() {
     OPENSSL_init_crypto(OPENSSL_INIT_ENGINE_ALL_BUILTIN | OPENSSL_INIT_LOAD_CONFIG, NULL);
+    SSL_CTX *ctx;
+    SSL *ssl;
+    // Инициализация SSL контекста
+    ctx = create_ssl_context();
+    
+    // Создание SSL структуры
+    ssl = SSL_new(ctx);
 
-    SSL_library_init();
-    SSL_load_error_strings();
-    ENGINE_load_builtin_engines();
+    // Установка соединения (например, установка сокета и вызов SSL_connect())
 
-    OpenSSL_add_all_algorithms();
-    OpenSSL_add_all_ciphers();
+    // Отправка/получение данных через SSL соединение (например, с помощью SSL_read() и SSL_write())
 
-    // Инициализация вашего собственного 
-    const EVP_CIPHER *cipher_belt_dwp128 = EVP_get_cipherbyname("belt-dwp128");
-    if (cipher_belt_dwp128)
-    {
-        printf("Algorithm belt-dwp128 is available.\n");
-    }
-    else
-    {
-        printf("Algorithm belt-dwp128 is not available.\n");
-    }
-
-    // Проверка наличия алгоритма belt-dwp192
-    const EVP_CIPHER *cipher_belt_dwp192 = EVP_get_cipherbyname("belt-dwp192");
-    if (cipher_belt_dwp192)
-    {
-        printf("Algorithm belt-dwp192 is available.\n");
-    }
-    else
-    {
-        printf("Algorithm belt-dwp192 is not available.\n");
-    }
-
-    // Проверка наличия алгоритма belt-dwp256
-    const EVP_CIPHER *cipher_belt_dwp256 = EVP_get_cipherbyname("belt-dwp-tls");
-    if (cipher_belt_dwp256)
-    {
-        printf("Algorithm belt-dwp256 is available.\n");
-    }
-    else
-    {
-        printf("Algorithm belt-dwp256 is not available.\n");
-    }
-    // Создать SSL_CTX с вашим собственным движком
-    SSL_CTX *ctx = SSL_CTX_new(SSLv23_server_method());
-
-    if (!ctx)
-    {
-        fprintf(stderr, "Failed to create SSL_CTX.\n");
-        return 1;
-    }
-
-    // Установить ваш собственный движок в SSL_CTX
-
-    if (!SSL_CTX_set_cipher_list(ctx, "DHE-BIGN-WITH-BELT-DWP-HBELT"))
-    {
-        fprintf(stderr, "Failed to set cipher list.\n");
-        SSL_CTX_free(ctx);
-        return 1;
-    }
-    // Дополнительная конфигурация SSL_CTX (если необходимо)
-    // ...
-
-    // Использовать SSL_CTX для создания SSL объекта и дальнейшей настройки соединения
-    // ...
-
-    // Освободить ресурсы
+    // Закрытие соединения и освобождение ресурсов
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
     SSL_CTX_free(ctx);
-    ENGINE_cleanup();
-    EVP_cleanup();
 
     return 0;
 }
