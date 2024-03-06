edc380cacfc2859775e41219620d48f2a17e79f8
obrovec913 <boroda9133@yandex.by>
2024-03-06 15:09:06 +0300

    first commit

---
+++ build00.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+gcc -o tunnel_malidi_server tunnelc.c -L/opt/beltunnel/build/local/lib/ -I/opt/beltunnel/build/local/include/ -lssl -lcrypto -lbee2evp -lpthread -Wl,-rpath=/opt/beltunnel/build/local/lib/
+mkdir -p package/usr/bin
+mkdir -p package/opt/beltunnel/build/local/lib
+mkdir -p package/opt/beltunnel/build/local/include
+
+cp tunnel_malidi_server package/usr/bin/
+cp /opt/beltunnel/build/local/lib/* package/opt/beltunnel/build/local/lib/
+cp /opt/beltunnel/build/local/include/* package/opt/beltunnel/build/local/include/
+
+# Создание файла control
+echo "Package: tunnel_malidi" > package/DEBIAN/control
+echo "Version: 1.0" >> package/DEBIAN/control
+echo "Architecture: amd64" >> package/DEBIAN/control
+echo "Maintainer: Your Name <your.email@example.com>" >> package/DEBIAN/control
+echo "Description: Description of your program" >> package/DEBIAN/control
+echo " A longer description can go here if needed." >> package/DEBIAN/control
+
+dpkg-deb --build package
