9da1799b770fb918fdebeaf40425fb639293cc08
obrovec913 <boroda9133@yandex.by>
2024-03-06 16:05:39 +0300

    first commit

--- build00.sh
+++ build00.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-gcc -o tunnel_malidi_server tunnelc.c -L/opt/beltunnel/build/local/lib/ -I/opt/beltunnel/build/local/include/ -lssl -lcrypto -lbee2evp -lpthread -Wl,-rpath=/opt/beltunnel/build/local/lib/
+gcc -o tunnel_malidi_client tunneld.c -L/opt/beltunnel/build/local/lib/ -I/opt/beltunnel/build/local/include/ -lssl -lcrypto -lbee2evp -lpthread -Wl,-rpath=/opt/beltunnel/build/local/lib/
 mkdir -p package/usr/bin
+mkdir -p package/DEBIAN
 mkdir -p package/opt/beltunnel/build/local/lib
 mkdir -p package/opt/beltunnel/build/local/include
 
-cp tunnel_malidi_server package/usr/bin/
+cp tunnel_malidi_client package/usr/bin/
 cp -r /opt/beltunnel/build/local/lib/* package/opt/beltunnel/build/local/lib/
 cp -r /opt/beltunnel/build/local/include/* package/opt/beltunnel/build/local/include/
 
 # Создание файла control
-echo "Package: tunnel_malidi" > package/DEBIAN/control
+echo "Package: tunnelMalidi" > package/DEBIAN/control
 echo "Version: 1.0" >> package/DEBIAN/control
 echo "Architecture: amd64" >> package/DEBIAN/control
-echo "Maintainer: Your Name <your.email@example.com>" >> package/DEBIAN/control
+echo "Maintainer: Your Name <your.email@malidi.by>" >> package/DEBIAN/control
 echo "Description: Description of your program" >> package/DEBIAN/control
 echo " A longer description can go here if needed." >> package/DEBIAN/control
 
